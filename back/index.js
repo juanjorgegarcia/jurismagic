82639217a9784e0881662020ae01e1ad19c99a23
juanjorgegarcia <juan.j.garcia007@gmail.com>
2018-07-09 03:11:50 -0300

    second

---
+++ back/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const port = 5000
+
+let con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database : 'jurisprudencia_2_inst'
+})
+
+con.connect()
+
+app.get('/q', function(req, res, next) {
+    console.log(req.query.text)
+    const sql = `SELECT * FROM jurisprudencia_2_inst WHERE texto_decisao LIKE '%${req.query.text}% LIMIT 5'`
+    con.query(sql, function (err, result) {
+    if (err) throw err
+    res.json(result)
+    })
+})
+
+app.listen(port, 'localhost', () => {
+    console.log(`server started @ localhost:${port}`)
+})
+
+// module.exports = app;
---
+++ jurismagic/src/App.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { TextField, Icon, InputAdornment, Button } from '@material-ui/core'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      query: "",
+      results: null
+    }
+  }
+
+  
+  onQuerySubmit = async () => {
+    if (this.state.query === '') {
+      return false
+    }
+    
+    let params = new URLSearchParams(Object.entries({
+      text: this.state.query
+    }))
+
+    let res = await fetch('/q?'+params, {
+      method: 'get'
+    })
+
+    res = await res.json()
+    this.setState({ results: res })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title"> JurisMagic </h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <TextField
+          id="search"
+          label="Search field"
+          type="search"
+          margin="normal"
+          value={this.state.query}
+          onChange={(ev) => {
+            this.setState({ query: ev.target.value })
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Icon>search</Icon>
+              </InputAdornment>
+            ),
+          }}
+        />
+      <br/>
+      <Button variant="contained" color="primary" onClick={this.onQuerySubmit}>
+        Pesquisar
+        <Icon style={{marginLeft: 5}}>send</Icon>
+      </Button>
+
+      {this.state.results
+        ? this.state.results.map((r,i) => <p key={'result'+i}> {r.texto_decisao} </p>)
+        : null
+      }
+
+      {this.state.results instanceof Array && this.state.results.length === 0 ? <p> Não achei nada fera </p> : null} 
+
+      </div>
+    );
+  }
+}
+
+export default App;
