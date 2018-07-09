d98761e54e42eab5870ecc0fc1e0b4f50e09a6c0
Fabio <fabioja@insper.edu.br>
2018-07-09 07:51:10 +0000

    conexao com o sql da aws feita

--- back/index.js
+++ back/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const port = 5000
+const credentials = require ('./credentials')
 
 let con = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  password: "",
+  user: credentials.user,
+  password: credentials.pass,
   database : 'jurisprudencia_2_inst'
 })
 
@@ -14,9 +15,10 @@ con.connect()
 
 app.get('/q', function(req, res, next) {
     console.log(req.query.text)
-    const sql = `SELECT * FROM jurisprudencia_2_inst WHERE texto_decisao LIKE '%${req.query.text}% LIMIT 5'`
+    const sql = `SELECT * FROM jurisprudencia_2_inst WHERE texto_decisao LIKE '%${req.query.text}%' LIMIT 5`
     con.query(sql, function (err, result) {
     if (err) throw err
+    console.log(result)
     res.json(result)
     })
 })
